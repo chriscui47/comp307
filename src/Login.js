89657958d5d2f85f68987426f0a2bbae491ed60c
lbaier <luc.baier-reinio@mail.mcgill.ca>
2022-03-27 21:06:52 -0400

    adding link redirection

--- src/Login.js
+++ src/Login.js
@@ -11,63 +11,51 @@ function Login() {
 
   const userNameRef= useRef();
   const passWordRef = useRef();
-  function submitHandler(event) {
-    event.preventDefault();
-
-    const enteredUserName = userNameRef.current.value;
-    const enteredPassWord = passWordRef.current.value;
-
-    const userData = {
-      username: enteredUserName,
-      password: enteredPassWord,
-    }
-    console.log(userData);
-    // Send to server to store in DB. (HTTP Request)
-   fetch('https://ta-management-47.herokuapp.com/api/user/login', {
-     // Configure
-     method: 'POST',
-     body: JSON.stringify(userData),
-     headers: {
-       'Content-Type': 'application/json' // Denote we are sending JSON data.
-     }
-
-
-   }).then(response => console.log(response)
-
-   ); 
-
-   // Conditionally redirect to different page here. 
-   //redirect
-
-
-  }
 
   return (
     <div className={styles.register}>
-      <form onSubmit={submitHandler}>
+      <form>
         Username <br />
         <input type="text" required id='username' ref={userNameRef}></input> <br />
         Password <br />
         <input type="text" required id='password' ref={passWordRef}></input>
         <br />  <br />
 
+        <Link onClick={ e => {
+          const enteredUserName = userNameRef.current.value;
+          const enteredPassWord = passWordRef.current.value;
+            const userData = {
+            username: enteredUserName,
+            password: enteredPassWord,
+            }
+            // Send to server to store in DB. (HTTP Request)
+           fetch('https://ta-management-47.herokuapp.com/api/user/login', {
+             // Configure
+             method: 'POST',
+             body: JSON.stringify(userData),
+             headers: {
+               'Content-Type': 'application/json' // Denote we are sending JSON data.
+             }
+           }).then(
+             response =>
+              {
+                if (response.status===404) {
+                  // Should prevent event here.
+                  e.preventDefault();
+                }
+                else {
+                  return null;
+                }
+              });
+             
+
+        } }to={"/register"}>
         <button>Login</button>
+        </Link>
         
       </form>
     </div>
   );
 }
-/**
- *  <Link onClick={
-          e => {
-            if (1 > 0) {
-              e.preventDefault();
-              alert("Enter your username before creating lobby");
-            }
-            return null}
 
-        } to={"/register"}>
-        <button>Login</button>
-        </Link>
- */
 export default Login;
