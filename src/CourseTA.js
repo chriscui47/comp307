e7011ad6d56fef2043429aa6ab73bbba7d24488d
lbaier <luc.baier-reinio@mail.mcgill.ca>
2022-04-01 15:08:03 -0400

    adding TA registering unregistering

--- src/CourseTA.js
+++ src/CourseTA.js
@@ -13,20 +13,19 @@ async function get(url){
     }
 }
 
-
 function CourseTA(props) {
     var existingTAs = [];
     const [TAs, setTAs] = useState([]);
     const [showTAs, setShowTAs] = useState(false);
-    useEffect(() => {
-        get("for allusers").then(response => {setTAs(response)} );
+    useEffect(() => { // Get all users, filter by TAs.
+        get("https://ta-management-47.herokuapp.com/api/user").then(response => {setTAs(response)} );
         props.users.filter(user => user.role_name.charAt(2)==1)
         .forEach(user => existingTAs.push(user.id));
       }, []); 
     return (
     <li>
         
-        <button className={styles.course} onClick="displayTAs">
+        <div className={styles.course} >
         <div>
             <h3>Course Code: {props.code} </h3>
         </div>
@@ -37,31 +36,23 @@ function CourseTA(props) {
             <h3>Semester: {props.term} </h3> 
         </div>
         <button onClick={
-            function() {
-                if (showTAs) {
-                    setShowTAs(false);
-                    return;
-                }
-                setShowTAs(true);
-            }
-        }>Edit TAs</button> 
-        {showTAs && <ul>
+                        function() {
+                            setShowTAs(!showTAs);
+                    }}>Edit TAs</button> <div className={styles.msg}>Note selected TAs are currently TAs for this class.</div>
+       
+       </div>
+       {showTAs && <div className={styles.dropdown}>
+       <ul>
         { // Add TAs that do exist in this course already, denote that they have been checked.
-            props.users.map(user => <TA fname={user.first_name} lname={user.last_name} checked={true}/>) 
-            
-            
+            props.users.map(user => <TA fname={user.first_name} lname={user.last_name} checked={true}/>)     
         }
         {   // Get all remaining TAs, denote that they haven't been checked. 
             TAs.filter(user => user.role_name.charAt(2)==1 && !existingTAs.includes(user.id)) // Add rest of TAs
             .map(user => <TA fname={user.first_name} lname={user.last_name} checked={false}/>)
         }
         </ul>
-        }   
-       </button>
-      
-        
+          
+       </div>}
     </li>);
-    // Upon selecting course go somewhere.
 }
-
 export default CourseTA;
--- src/TA.js
+++ src/TA.js
@@ -1,14 +1,20 @@
 import styles from "./TA.module.css";
-
+import React, { useState } from 'react';
+import { useEffect } from 'react';
 function TA(props) {
     return ( // Use student ID to get courses.
-    <li>
-        <div>
-            {props.fname} {props.lname} 
-       
-        <input type="checkbox" id="myCheck" defaultChecked={props.checked} onclick="myFunction()"></input>
-        <button>Submit Changes</button>
+    <li className={styles.screen}>
+
+            <div className={styles.parent} >
+            <div className={styles.name}>
+            {props.fname} {props.lname}
+            </div> 
+            <div className={styles.box}>
+            <input type="checkbox" id={props.id} defaultChecked={props.checked}></input>  
+            </div>
         </div>
+
+
         </li>
     )
 }
